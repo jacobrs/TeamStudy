4b865596959afa5fe555e592cabe11ae33457039
Simon Bourque <simon.bourque@live.ca>
2017-01-28 14:37:07 -0500

    Add temporary button to link to profile page

---
+++ client/modules/User/pages/UserProfilePage/UserProfilePage.js
@@ -0,0 +1,47 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+
+// Import Components
+//import RegistrationForm from '../../components/UserRegistrationForm/UserRegistrationForm';
+
+// Import Actions
+import { addUserRequest } from '../../UserActions';
+
+export default class UserProfilePage extends Component {
+  // we aren't loading any data yet.
+  //componentDidMount() {
+  //  this.props.dispatch([]);
+  //}
+
+  //handleAddUser = (firstName, lastName, email) => {
+    //this.props.dispatch(addUserRequest({ firstName, lastName, email }));
+  //};
+
+  render() {
+    return (
+      <div>
+        <h1>Profile page will be here</h1>
+      </div>
+    );
+  }
+}
+
+// Retrieve data from store as props
+//function mapStateToProps() {
+  //return {};
+//}
+
+//UserRegistrationPage.propTypes = {
+  //users: PropTypes.arrayOf(PropTypes.shape({
+    //firstName: PropTypes.string.isRequired,
+    //lastName: PropTypes.string.isRequired,
+    //email: PropTypes.string.isRequired,
+  //})),
+  //dispatch: PropTypes.func.isRequired,
+//};
+
+//UserRegistrationPage.contextTypes = {
+  //router: React.PropTypes.object,
+//};
+
+//export default connect(mapStateToProps)(UserRegistrationPage);
