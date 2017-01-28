c3f4fd744d62017cae74ead22eefe1f1e22eeda9
Simon Bourque <simon.bourque@live.ca>
2017-01-28 14:45:52 -0500

    Remove commented out code blocks

--- client/modules/User/pages/UserProfilePage/UserProfilePage.js
+++ client/modules/User/pages/UserProfilePage/UserProfilePage.js
@@ -1,22 +1,6 @@
-import React, { PropTypes, Component } from 'react';
-import { connect } from 'react-redux';
-
-// Import Components
-//import RegistrationForm from '../../components/UserRegistrationForm/UserRegistrationForm';
-
-// Import Actions
-import { addUserRequest } from '../../UserActions';
+import React, { Component } from 'react';
 
 export default class UserProfilePage extends Component {
-  // we aren't loading any data yet.
-  //componentDidMount() {
-  //  this.props.dispatch([]);
-  //}
-
-  //handleAddUser = (firstName, lastName, email) => {
-    //this.props.dispatch(addUserRequest({ firstName, lastName, email }));
-  //};
-
   render() {
     return (
       <div>
@@ -26,22 +10,3 @@ export default class UserProfilePage extends Component {
   }
 }
 
-// Retrieve data from store as props
-//function mapStateToProps() {
-  //return {};
-//}
-
-//UserRegistrationPage.propTypes = {
-  //users: PropTypes.arrayOf(PropTypes.shape({
-    //firstName: PropTypes.string.isRequired,
-    //lastName: PropTypes.string.isRequired,
-    //email: PropTypes.string.isRequired,
-  //})),
-  //dispatch: PropTypes.func.isRequired,
-//};
-
-//UserRegistrationPage.contextTypes = {
-  //router: React.PropTypes.object,
-//};
-
-//export default connect(mapStateToProps)(UserRegistrationPage);
