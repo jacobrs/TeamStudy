b9b039126cc8bf3d48aae44c02df989a74f8ec47
Dylan Fernandes <dylangfernandes@gmail.com>
2017-02-02 15:35:29 -0500

    #49 functional tests for static testing

--- client/modules/User/__test__/apiUsers.spec.js
+++ client/modules/User/__test__/apiUsers.spec.js
@@ -1,35 +1,25 @@
 import test from 'ava';
-import callApi, { API_URL } from '../../../util/apiCaller';
-import nock from 'nock';
+import { actionTest } from 'redux-ava';
 
-test('sends the body', t => {
-  const body = { id: 5 };
-  const user = { nickname: 'foo bar', studentId: '12345678', email: 'test@test.com', password: 'password123' };
-  nock(API_URL)
-    .post('/registration', user)
-    .reply(200, user);
-  return callApi('user', 'post', user).then(response => {
-    t.deepEqual(response, user);
-  });
-});
+import {
+  ADD_USER,
+  LOGIN_USER,
+  addUser,
+  loginUser,
+} from '../UserActions';
 
-// test('method defaults to GET', t => {
-//   const reply = { users: '{"_id":"589201f691293665a65538df","cuid":"ciyn4gkh900002us7yyn1nejq","firstName":"Dylan","lastName":"Fernandes","studentId":40002559,"email":"dylangfernandes@gmail.com","password":"bed4efa1d4fdbd954bd3705d6a2a78270ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c790de74e17ecfec3cb6ac4bf","__v":0,"lastLogin":null,"dateAdded":"2017-02-01T15:42:46.687Z"}' };
-//   nock(API_URL)
-//     .get('api/users')
-//     .reply(200, reply);
-//   return callApi('foo').then(response => {
-//     t.deepEqual(response, reply);
-//   });
-// });
+const user = {"_id":"589201f691293665a65538df","cuid":"piyn4gkh900002us7yyn1nejq","firstName":"Tester","lastName":"Testee","studentId":12345678,"email":"dylangfernandes@gmail.com","password":"bed4efa1d4fdbd954bd3705d6a2a78270ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c790de74e17ecfec3cb6ac4bf","__v":0,"lastLogin":null,"dateAdded":"2017-02-01T15:42:46.687Z"}
 
+test('should return the correct type for addUser', actionTest(
+  addUser,
+  user,
+  { type: ADD_USER, user },
 
-// test('returns the error', t => {
-//   const reply = { message: 'Errrrrrrrrr' };
-//   nock(API_URL)
-//     .get('/send_error')
-//     .reply(500, reply);
-//   return callApi('send_error').then(error => {
-//     t.deepEqual(error, reply);
-//   });
-// });
+));
+
+test('should return the correct type for loginUser', actionTest(
+  loginUser,
+  user.cuid,
+  { type: LOGIN_USER, response: user.cuid},
+));
+
