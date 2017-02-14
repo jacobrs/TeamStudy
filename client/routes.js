acc10904a19b2fc9d5c8c076c984e16a3b23be3a
pbgnz <pabgonza1995@gmail.com>
2017-02-14 16:49:37 -0500

    renamed forder to UserDashboardPage

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/App/App';
-import UserDashboardPage from './modules/User/pages/UserRefactorPage/UserDashboardPage';
+import UserDashboardPage from './modules/User/pages/UserDashboardPage/UserDashboardPage';
 import UserRegistrationPage from './modules/User/pages/UserRegistrationPage/UserRegistrationPage';
 
 // require.ensure polyfill for node
