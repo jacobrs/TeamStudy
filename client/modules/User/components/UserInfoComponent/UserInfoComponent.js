b34359911bd6fa0efea55ed4a4ea547af66b123e
pbgnz <pabgonza1995@gmail.com>
2017-02-15 11:28:48 -0500

    #41 component done, needs styling though

--- client/modules/User/components/UserInfoComponent/UserInfoComponent.js
+++ client/modules/User/components/UserInfoComponent/UserInfoComponent.js
@@ -1,22 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-import { injectIntl } from 'react-intl';
 
 // Import Style
 // import styles from './UserInfoComponent.css';
 
-export class UserInfoComponent extends Component {
-  render() {
-    console.log("UserInfoComponent")
-    console.log(this.props.users)
+function UserInfoComponent(props) {
+  if(Object.keys(props.users).length == 1) // wait props.users not to be null
     return (
-      <h2>{this.props.users.firstName}</h2>
+      <div>
+        <h2>Welcome {props.users.user.firstName} {props.users.user.lastName}</h2>
+        <h3>Account created {props.users.user.dateAdded}</h3>
+      </div>
     );
-  }
+  return (
+    <h1>Loading</h1>
+  )
 }
 
-// UserRegistrationForm.propTypes = {
-//   // addUser: PropTypes.func.isRequired,
-//   // intl: intlShape.isRequired,
-// };
 
-export default injectIntl(UserInfoComponent);
+export default UserInfoComponent;
