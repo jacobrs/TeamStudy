e91072a2d610bfbbacd2a0f4bcdb9b5d4268639d
Martin <martinas95@yahoo.fr>
2017-02-15 19:15:44 -0500

    Added sidebar component

--- client/modules/User/components/UserInfoComponent/UserInfoComponent.js
+++ client/modules/User/components/UserInfoComponent/UserInfoComponent.js
@@ -1,14 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 
 // Import Style
-// import styles from './UserInfoComponent.css';
+import styles from './UserInfoComponent.css';
 
 function UserInfoComponent(props) {
   if(Object.keys(props.users).length == 1) // wait props.users not to be null
     return (
-      <div>
-        <h2>Welcome {props.users.user.firstName} {props.users.user.lastName}</h2>
-        <h3>Account created {props.users.user.dateAdded}</h3>
+      <div className="col-md-3" id={styles.sidebar}>
+        <img className="img-circle" id={styles['circle-image']}src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2015/06/15/09/jon-snow.jpg" />
+        <h3>{props.users.user.firstName}  {props.users.user.lastName} </h3>
+        <p>{props.users.user.email} </p>
+        <ul>
+          <li><a href="#">Course 1</a></li>
+          <li><a href="#">Course 2</a></li>
+          <li><a href="#">Course 3</a></li>
+          <li><a href="#">Course 4</a></li>
+          <li><a href="#">Course 5</a></li>
+        </ul>
       </div>
     );
   return (
@@ -18,3 +26,5 @@ function UserInfoComponent(props) {
 
 
 export default UserInfoComponent;
+
+
