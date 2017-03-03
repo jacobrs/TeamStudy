e035f3321049d09068f3def0a40cb16be6dcb0e5
Samantha Kerr <samantha-kerr@hotmail.ca>
2017-03-03 14:07:29 -0500

    wip #79 implemented study group component, contains placeholder links

--- client/modules/User/components/UserStudyGroupComponent/UserStudyGroupComponent.js
+++ client/modules/User/components/UserStudyGroupComponent/UserStudyGroupComponent.js
@@ -8,8 +8,9 @@ function UserStudyGroupComponent(props){
   if(Object.keys(props.users).length === 1){
     return (
       <div className={styles.studyGroup}>
+        <h4>Study Groups</h4>
         <ul>
-          <li><Link href="">ENGR 233</Link></li>
+          <li><Link href="">SOEN 331</Link></li>
           <li><Link href="">SOEN 341</Link></li>
           <li><Link href="">ELEC 275</Link></li>
         </ul>
