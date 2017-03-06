bebaf163c0551fbb8d5ef20fc795beaacaddf715
Dylan Fernandes <dylangfernandes@gmail.com>
2017-03-06 15:25:09 -0500

    #104 inserted tests for authentication and login

--- client/modules/User/__test__/apiRegistration.spec.js
+++ client/modules/User/__test__/apiRegistration.spec.js
@@ -4,9 +4,14 @@ import { actionTest } from 'redux-ava';
 import {
   ADD_USER,
   addUser,
+  AUTHENTICATE_SESSION,
+  authenticateSession,
+  LOGOUT_USER,
+  logoutUser
 } from '../UserActions';
 
 const user = { '_id': '589201f691293665a65538df', 'cuid': 'piyn4gkh900002us7yyn1nejq', 'firstName': 'Tester', 'lastName': 'Testee', 'studentId': 12345678, 'email': 'dylangfernandes@gmail.com', 'password': 'bed4efa1d4fdbd954bd3705d6a2a78270ec9a52ecfbfb010c61862af5c76af1761ffeb1aef6aca1bf5d02b3781aa854fabd2b69c790de74e17ecfec3cb6ac4bf', '__v': 0, 'lastLogin': null, 'dateAdded': '2017-02-01T15:42:46.687Z' };
+const response = { 'statusCode': 200};
 
 test('should return the correct type for addUser', actionTest(
   addUser,
@@ -15,3 +20,16 @@ test('should return the correct type for addUser', actionTest(
 
 ));
 
+test('should return the correct type for authenticateSession', actionTest(
+  authenticateSession,
+  response,
+  response.cuid
+
+));
+
+test('should return the correct type for logoutUser', actionTest(
+  logoutUser,
+  response,
+   response.cuid
+
+));
