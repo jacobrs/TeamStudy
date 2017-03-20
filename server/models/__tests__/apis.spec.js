86852e614b84f5a5ab8a8b50ad323346c56c422c
pbgnz <pabgonza1995@gmail.com>
2017-03-19 20:45:10 -0400

    #66 test API user and studygroup endpoints

---
+++ server/models/__tests__/apis.spec.js
@@ -0,0 +1,65 @@
+import test from 'ava';
+import request from 'supertest';
+import app from '../../server';
+
+//*************
+// User
+//*************
+
+test.serial('Should correctly create a new User', async t => {
+  t.plan(5);
+  const res = await request(app)
+    .post('/api/users')
+    .send({ user: {firstName: 'Naruto', lastName: 'Uzumaki', studentId: '77777777', email: 'naruto@gmail.com', password: 'f34gb2bh24b24b3'}})
+    .set('Accept', 'application/json');
+  t.is(res.status, 200);
+  t.is(res.body.user.firstName, 'Naruto');
+  t.is(res.body.user.lastName, 'Uzumaki');
+  t.is(res.body.user.studentId, 77777777);
+  t.is(res.body.user.email, 'naruto@gmail.com');
+});
+
+test.serial('Should correctly login the User', async t => {
+  t.plan(5);
+  const res = await request(app)
+    .post('/api/users/login')
+    .send({email: 'naruto@gmail.com', password: 'f34gb2bh24b24b3'})
+    .set('Accept', 'application/json');
+  t.is(res.status, 200);
+  t.is(res.body.user.firstName, 'Naruto');
+  t.is(res.body.user.lastName, 'Uzumaki');
+  t.is(res.body.user.studentId, 77777777);
+  t.is(res.body.user.email, 'naruto@gmail.com');
+});
+
+test.serial('Should correctly logoff the User', async t => {
+  t.plan(2);
+  const res = await request(app)
+    .get('/api/users/logout')
+    .set('Accept', 'application/json');
+  t.is(res.status, 200);
+  t.is(res.body.user, null);
+});
+
+//*************
+// StudyGroups
+//*************
+
+test.serial('Should correctly create a new Study Group', async t => {
+  t.plan(5);
+  const res = await request(app)
+    .post('/api/studyGroups')
+    .send({ studyGroup: {groupName: 'Hanna Boys', course: 'comp346', teacher: 'Hanna Aiman', description: 'Operating Systems'}})
+    .set('Accept', 'application/json');
+  t.is(res.status, 200);
+  t.is(res.body.studyGroup.groupName, 'Hanna Boys');
+  t.is(res.body.studyGroup.course, 'comp346');
+  t.is(res.body.studyGroup.teacher, 'Hanna Aiman');
+  t.is(res.body.studyGroup.description, 'Operating Systems');
+});
+
+//*************
+// Messages
+//*************
+
+
