b1e3983bf3d8b408807696e8f802485f857720a1
Samantha Kerr <samantha-kerr@hotmail.ca>
2017-03-29 17:31:28 -0400

    done #79 Component displays user's study groups, links them to chat component of each study group

--- client/modules/User/components/UserStudyGroupComponent/UserStudyGroupComponent.js
+++ client/modules/User/components/UserStudyGroupComponent/UserStudyGroupComponent.js
@@ -5,30 +5,32 @@ import { fetchUserGroups } from '../../UserActions'
 // Import style
 import styles from './UserStudyGroupComponent.css';
 
-function UserStudyGroupComponent(props){
-  const arr = [
-    { name: 'Dream Team Study' },       // Mock data (study groups)
-    { name: 'SOEN 341 Study Group' },
-    { name: 'COMP 346 Exam Study' },
-    { name: 'Team "Mandy\'s Salad" Discussion' },
-  ];
-
-  if (props.users.user !== null) {
-    return (
-      <div className={styles.studyGroup}>
-        <h4>Study Groups</h4>
-        <ul>
-          {(arr).map((group, i) => {    // Displays group name in array of user study groups. Tested using mock data.
-            return <li key={i}><Link href="">{group.name}</Link></li>;    // Missing routes to chat.
-          })}
-        </ul>
-      </div>
-    );
+export class UserStudyGroupComponent extends Component{
+  constructor(props){
+    super(props);
   }
 
-  return (
-    <h1>Loading</h1>
-  );
+  handleClick (i, event){
+    this.props.setChat(i);
+  }
+
+  render(){
+    if ((this.props.users.user.studyGroups).length !== 0) {
+      return (
+        <div className={styles.studyGroup}>
+          <h4>Study Groups</h4>
+          <ul>
+            {(this.props.users.user.studyGroups).map((group, i) => {
+              return <li key={i} onClick={this.handleClick.bind(this, i)}>{group.groupName}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    }
+    return (
+      <h1>Loading</h1>
+    );
+  }
 }
 
 export default UserStudyGroupComponent;
